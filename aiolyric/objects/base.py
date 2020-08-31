f6d9b51c869fd7d8bca06f8a55fb689c33080b0a
Aidan Timson <contact@timmo.xyz>
2020-08-31 15:00:41 +0100

    Move device to device

--- aiolyric/objects/base.py
+++ aiolyric/objects/base.py
@@ -5,8 +5,17 @@ import logging
 class LyricBase:
     """Base class for Lyric."""
 
-    logger = logging.getLogger("Lyric")
+    logger = logging.getLogger("AIOLyric")
 
     def __init__(self, attributes) -> None:
         """Initialize."""
         self.attributes = attributes
+
+
+class LyricBaseClient(LyricBase):
+    """Base class for Lyric."""
+
+    def __init__(self, client: "AIOGitHubAPIClient", attributes: dict) -> None:
+        """Initialise."""
+        super().__init__(attributes)
+        self.client = client
