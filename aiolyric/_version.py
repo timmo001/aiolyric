b106c56906733b50fc95df1c9ebfcd6ab92666b6
Aidan Timson <aidan@timmo.dev>
2024-04-01 11:53:45 +0100

    Update setup

---
+++ aiolyric/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides aiolyric version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update aiolyric` to change this file.
+
+from incremental import Version
+
+__version__ = Version("aiolyric", 1, 1, 2, dev=0)
+__all__ = ["__version__"]
--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
-#!/usr/bin/env python
-import io
-
+"""Setup."""
 from setuptools import find_packages, setup
 
-version = "1.1.1"
+# Get setup packages from requirements.txt
+with open("requirements_setup.txt", encoding="utf-8") as f:
+    requirements_setup = f.read().splitlines()
+
+# Get packages from requirements.txt
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
 
 setup(
     name="aiolyric",
-    version=version,
-    description="AIO package for the Honeywell Lyric Platform.",
-    long_description=io.open("README.md", encoding="UTF-8").read(),
-    keywords="honeywell lyric thermostat",
     author="Aidan Timson (Timmo)",
-    author_email="contact@timmo.xyz",
-    license="MIT",
+    author_email="aidan@timmo.dev",
+    description="Python package for the Honeywell Lyric Platform",
+    keywords="aiolyric,api,async,asyncio,honeywell lyric,honeywell,lyric,integration",
+    license="Apache-2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/timmo001/aiolyric",
+    install_requires=requirements,
     packages=find_packages(exclude=["tests", "generator"]),
-    install_requires=["aiohttp>=3.7.3"],
+    python_requires=">=3.11",
+    setup_requires=requirements_setup,
+    use_incremental=True,
 )
